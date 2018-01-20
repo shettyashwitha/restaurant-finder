fca03d8be4c86e81f4a9bea213493c2b8462e864
ashwitha shetty <ashwitha.shetty@tandf.co.uk>
2018-01-20 20:18:36 +0530

    book table added

--- src/restaurant/restaurant.js
+++ src/restaurant/restaurant.js
@@ -7,24 +7,24 @@ class Restaurant extends Component {
   }
   render() {
     const restaurant = this.props.data.restaurant;
-    console.log(restaurant);
+    const positiveRatings = Math.floor(restaurant.user_rating.aggregate_rating);
+    const noRatings = 5 - positiveRatings;
+    console.log(positiveRatings, noRatings);
     return (
         <div className = "restaurant-each-overlay">
           <img src={restaurant.thumb} alt={restaurant.name} className="restaurant-image"/>
           <div className="right-section">
             <div className="restaurant-name">{restaurant.name}<span>location address</span></div>
             <div className="user-rating"><span className="med-text">User rating: </span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star checked"></span>
-              <span className="fa fa-star"></span>
+              {[...Array(positiveRatings)].map((e, i) => <span className="fa fa-star checked" key={i}></span>)}
+              {[...Array(noRatings)].map((e, i) => <span className="fa fa-star" key={i}></span>)}
             </div>
             <div className="small-text margin-down">{restaurant.user_rating.aggregate_rating} average based on {restaurant.user_rating.votes} reviews.</div>
             <div className="f15 margin-down"><span className="med-text">Address: </span>{restaurant.location.address}</div>
             <div className="f15 margin-down"><span className="med-text">Cuisines: </span>{restaurant.cuisines}</div>
             <div className="f15 margin-down"><span className="med-text">Price range: </span>{restaurant.price_range}</div>
             <div className="online-delivery small-text">Available for online delivery?<span className="available"> {restaurant.has_online_delivery === 0 ? 'Yes' : 'No'}</span></div>
+            {restaurant.has_table_booking === 1 ? <span className="button-book-table"><a href={restaurant.book_url}>BOOK</a></span> : ''}
           </div>
         </div>
     );
